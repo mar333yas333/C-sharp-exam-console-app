f4c15687063a0625a29f6f49e90ec8eb259c02ad
Marwan Ahmed <mar333yas333@gmail.com>
2026-01-27 17:35:19 +0200

    Added list on Subject.cs

--- Subject.cs
+++ Subject.cs
@@ -3,7 +3,7 @@ class Subject
     public static int ids = 0;
     public int Id;
     public string Subject_Name;
-    public 
+    List <Exam> Subject_Exam = new List <Exam>();
 
     public void Subject(subject)
 }
