a7ba83d04c48797494ae630f2dca5f80b0687942
Marwan Ahmed <mar333yas333@gmail.com>
2026-02-01 22:37:55 +0200

    Update Using_Views.cs

--- Using_Views.cs
+++ Using_Views.cs
@@ -3,7 +3,9 @@ class Using_Views
     public static void S_view(){
         string title = $"welcome student {current_User.Email}";
         string [] options=[
-            "my courses","my exam",history",
+            "my courses",
+            "my exam",
+            "history",
             "my info",
             "log out"
         ]
