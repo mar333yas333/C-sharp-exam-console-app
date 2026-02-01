5770952272169cbfacedb37219f69bba4eabf75a
Marwan Ahmed <mar333yas333@gmail.com>
2026-02-01 21:15:15 +0200

    Update Using_Views.cs

--- Using_Views.cs
+++ Using_Views.cs
@@ -2,6 +2,11 @@ class Using_Views
 {
     public static void S_view(){
         string title = $"welcome student {current_User.Email}";
+        string [] options=[
+            "my courses","my exam",history",
+            "my info",
+            "log out"
+        ]
     }
     public static void P_view(){
         
